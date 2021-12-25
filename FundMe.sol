b32065a500a2c9e16acec55f0054d1e6e7a2d5ca
Sumit Kumar Gupta <SumitDev.Tech@gmail.com>
2021-12-25 18:46:31 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,38 +1,58 @@
-// SPDX-License_Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0; //this help us to use all solidity version less than and upto 0.6.0
+pragma solidity >=0.6.6 <0.9.0;
 
-import "./SimpleStorage.sol"; //this is similar to copying the whole code from the "StorageFactory" file
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
-contract StorageFactory is SimpleStorage {
-    // Inheritance in solidity by using "is" (i.e inheriting all the functions and variables from "SimpleStorage")
-
-    SimpleStorage[] public simpleStorageArray;
-
-    function createSimpleStorageContract() public {
-        SimpleStorage simpleStorage = new SimpleStorage();
-        simpleStorageArray.push(simpleStorage);
+contract FundMe {
+    using SafeMathChainlink for uint256;
+    
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
     }
-
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
-        public
-    {
-        /* anytime we interact with contract we need two things:-
-          1. "address" of the contract we want to interact wiith
-          2. ABI or Application Binary interface
-        */
-        // SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        // simpleStorage.store(_simpleStorageNumber);
-        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(
-            _simpleStorageNumber
-        );
+    
+    function fund() public payable {
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
-
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
-        // return simpleStorage.retrieve();
-        return
-            SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
-                .retrieve();
+    
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+    
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+         return uint256(answer * 10000000000);
+    }
+    
+    // 1000000000
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function withdraw() payable onlyOwner public {
+        msg.sender.transfer(address(this).balance);
+        
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
     }
 }
